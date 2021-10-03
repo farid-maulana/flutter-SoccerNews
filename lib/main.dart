4c4108102021525845462667cb43208a06b89082
farid-maulana <faridmaulana1921@gmail.com>
2021-10-03 23:13:18 +0700

    Tugas kedua flutter

--- lib/main.dart
+++ lib/main.dart
@@ -9,105 +9,235 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Soccer News',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('MyApp'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Text(
+                  'BERITA TERBARU',
+                  style: TextStyle(fontSize: 14),
+                ),
+                Text(
+                  'PERTANDINGAN HARI INI',
+                  style: TextStyle(fontSize: 14),
+                  ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 12),
+            color: Colors.purpleAccent,
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  'images/img1.jpg',
+                  fit: BoxFit.cover,
+                ),
+                Container(
+                  padding: EdgeInsets.all(12),
+                  width: double.infinity,
+                  color: Colors.white,
+                  child: Center(
+                    child: Text(
+                      'Costa Mendekat Ke Palmeiras',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(14),
+                  width: double.infinity,
+                  child: Text(
+                    'Transfer'
+                  ),
+                )
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(bottom: 8),
+                  padding: EdgeInsets.all(1),
+                  color: Colors.green,
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Image.asset(
+                            'images/img2.jpg',
+                            fit: BoxFit.contain,
+                            height: 115,
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(left: 10),
+                            margin: EdgeInsets.all(1),
+                            color: Colors.white,
+                            height: 115,
+                            width: 219,
+                            alignment: Alignment.centerLeft,
+                            child: Text('Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat'),
+                          )
+                        ],
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(14),
+                        width: double.infinity,
+                        color: Colors.white,
+                        child:Text(
+                          'Barcelona Feb 13, 2021',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 8),
+                  padding: EdgeInsets.all(1),
+                  color: Colors.green,
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Image.asset(
+                            'images/img2.jpg',
+                            fit: BoxFit.contain,
+                            height: 115,
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(left: 10),
+                            margin: EdgeInsets.all(1),
+                            color: Colors.white,
+                            height: 115,
+                            width: 219,
+                            alignment: Alignment.centerLeft,
+                            child: Text('Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat'),
+                          )
+                        ],
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(14),
+                        width: double.infinity,
+                        color: Colors.white,
+                        child:Text(
+                          'Barcelona Feb 13, 2021',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 8),
+                  padding: EdgeInsets.all(1),
+                  color: Colors.green,
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Image.asset(
+                            'images/img2.jpg',
+                            fit: BoxFit.contain,
+                            height: 115,
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(left: 10),
+                            margin: EdgeInsets.all(1),
+                            color: Colors.white,
+                            height: 115,
+                            width: 219,
+                            alignment: Alignment.centerLeft,
+                            child: Text('Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat'),
+                          )
+                        ],
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(14),
+                        width: double.infinity,
+                        color: Colors.white,
+                        child:Text(
+                          'Barcelona Feb 13, 2021',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 8),
+                  padding: EdgeInsets.all(1),
+                  color: Colors.green,
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Image.asset(
+                            'images/img2.jpg',
+                            fit: BoxFit.contain,
+                            height: 115,
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(left: 10),
+                            margin: EdgeInsets.all(1),
+                            color: Colors.white,
+                            height: 115,
+                            width: 219,
+                            alignment: Alignment.centerLeft,
+                            child: Text('Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat'),
+                          )
+                        ],
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(14),
+                        width: double.infinity,
+                        color: Colors.white,
+                        child:Text(
+                          'Barcelona Feb 13, 2021',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
     );
   }
 }
+
